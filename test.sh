c52ccd7157302aa490d70c1ef50be9ff9a9271ea
HoxiTree <53079057+junshutang@users.noreply.github.com>
2020-11-15 21:00:31 +0800

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,20 @@
+python3 main.py \
+	--data_dir ./data/DISFA/aligned_images/  \
+	--attr_dir ./data/DISFA/list_attr_disfa.txt \
+    --mode test \
+    --test_exc_path test_exc \
+    --test_epoch 15 \
+	--latent_dim 512 \
+	--batch_size 8 \
+	--train_shuffle True \
+	--log_dir log/  \
+	--test_src_num 0 \
+	--test_tgt_num 1 \
+	--lambda_au 10 \
+	--lambda_rec 100 \
+	--lambda_pe 10 \
+	--lambda_id 50 \
+	--lambda_ms 50 \
+	--n_epochs 16 \
+	--decay_epoch 8 \
+	--version v1
